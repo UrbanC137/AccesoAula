c46e0f6efc910c45bd3799bb5aab7fee1da0aafb
lexus210 <73870803@continental.edu.pe>
2025-06-05 10:49:09 -0500

    Actualización index

--- src/vista/index.php
+++ src/vista/index.php
@@ -41,7 +41,7 @@
                 <<!--Formulario de Login y registro-->
                 <div class="contenedor__login-register">
                     <!--Login-->
-                    <form action="php/login_usuario_be.php" method="POST" class="formulario__login">
+                    <form action="../control/login_usuario_be.php" method="POST" class="formulario__login">
                         <h2>Iniciar Sesión</h2>
                         <input type="text" placeholder="Correo Electronico" name="correo">
                         <input type="password" placeholder="Contraseña" name="contrasena">
